fe44b9ce437696db1b3e47ecea4ecb0635eaaa23
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-01-07 19:01:18 +0530

    httpreq updated

--- HTTPRequests/index.js
+++ HTTPRequests/index.js
@@ -3,10 +3,13 @@ const app=express();
 const port=3000;
 
 app.get("/",(req,res)=>{
- res.send("<h1>hello</h1>");
+ res.send("<h1>hello, this is Deepak</h1>");
 });
-app.get("/home",(req,res)=>{
-    res.send("<h1>hello home</h1>");
+app.get("/contact",(req,res)=>{
+    res.send("<h1>Contact me</h1> <p>deepak1122003kumar@gmail.com</p>");
+   });
+app.get("/about",(req,res)=>{
+    res.send("<h1>About</h1><p>This is Deepak from EEE department </p>");
    });
 app.listen(port,()=>{
     console.log(`server started in port ${port}`);
